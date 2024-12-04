7c3be4c4c454b74a9a6ffb116318c2b2aa3470dc
2470033 <131525115+ironmax123@users.noreply.github.com>
2024-12-04 16:45:18 +0900

    Update ds11.py

--- ds11.py
+++ ds11.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 import seaborn as sns
 
+# %%
+print(df.shape)
+print(df.columns)
+print(df.describe())
+
+# %%
 df = pd.read_excel("Banana_Quality.xlsx")
 
 sns.pairplot(df)
 plt.show()
 
 # %%
-print(df.shape)
-print(df.columns)
-print(df.describe())
 corr_matrix = df[['quality_score', 'ripeness_index', 'sugar_content_brix','firmness_kgf', 'length_cm', 'weight_g','tree_age_years', 'altitude_m', 'rainfall_mm', 'soil_nitrogen_ppm']].corr()
 
 sns.heatmap(corr_matrix,annot=True)
